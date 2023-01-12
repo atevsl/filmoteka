a04b451d2884f4b0187537659e7b0e6ee41a6ff9
atevsl <3380033@ukr.net>
2023-01-12 12:14:56 +0200

    copy

---
+++ src/js/dom/dom.js
@@ -0,0 +1,12 @@
+import './footerModal';
+import './inputSearch';
+import './modalLogIn';
+import './movieModal';
+import './refs';
+import './renderMovieModal';
+import './renderMovies';
+import './renderTrendingFilms';
+import './movieModal';
+import './trailer';
+import './watchedLS';
+import './queueLS';
